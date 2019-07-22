ea516ddc44d58ec72809ab7dc00d2b031b365134
ahj <740055963@qq.com>
2019-07-22 17:25:28 +0800

    初始项目提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,67 @@
+module.exports = {
+    base: '/vue-analysis/',
+    dest: 'dist',
+    title: 'Vue.js 技术揭秘',
+    description: 'Analysis vue.js deeply',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/logo.png`
+        }],
+        ['link', {
+            rel: 'manifest',
+            href: '/manifest.json'
+        }],
+        ['meta', {
+            name: 'theme-color',
+            content: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }],
+        ['link', {
+            rel: 'apple-touch-icon',
+            href: `/icons/apple-touch-icon-152x152.png`
+        }],
+        ['link', {
+            rel: 'mask-icon',
+            href: '/icons/safari-pinned-tab.svg',
+            color: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'msapplication-TileImage',
+            content: '/icons/msapplication-icon-144x144.png'
+        }],
+        ['meta', {
+            name: 'msapplication-TileColor',
+            content: '#000000'
+        }]
+    ],
+    serviceWorker: false,
+    themeConfig: {
+        // 自动在导航栏右侧添加，github链接跳转按钮，此为路径
+        repo: 'An-Haijun/js-advanced-technology-doc',
+        editLinks: true,
+        docsDir: 'docs',
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
+        nav: [{
+            text: '生态系统',
+            link: ''
+        }],
+        // 左侧菜单栏
+        sidebar: [{
+            title: '第一章 JavaScript 简介',
+            collapsable: false,
+            children: [
+                ['javascript-profile/', 'Introduction'],
+                'javascript-profile/brief-history'
+            ]
+        }]
+    }
+}
---
+++ docs/javascript-profile/index.md
@@ -0,0 +1,8 @@
+# JavaScript 简介
+
+**本章内容**
+
+- JavaScript 历史回顾
+- JavaScript 是什么
+
+JavaScript 诞生于 1995 年，当时，它的主要目的是处理以前由服务端语言（如 Perl）负责的没有填写某个必填域，是否输入了无效的值。
